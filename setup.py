2bc984e9394a1a21e76f7736835455734a09e03c
Germey <cqc@cuiqingcai.com>
2020-07-09 23:26:54 +0800

    py38

--- setup.py
+++ setup.py
@@ -112,6 +112,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
